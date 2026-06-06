5fe75c943de10c161f0d1419a60730008e27da98
Kweku <nyarkokweku353@gmail.com>
2026-06-06 21:33:08 +0000

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -7,3 +7,13 @@ print(f"{name} " * 10) #assignment3
 
 IP = input("Whats your IP ? ")
 print(f"Your IP is: {IP}") #assignment4
+
+print("Take a number.")
+first_number = int(input("Enter a number: "))
+second_number = int(input("Enter another number: "))
+if first_number > second_number:
+    print(f"{first_number} is greater than {second_number}.")
+elif first_number < second_number:
+    print(f"{second_number} is greater than {first_number}.")
+else:
+    print(f"{first_number} is equal to {second_number}.") #assignment5
