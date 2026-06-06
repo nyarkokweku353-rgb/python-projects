d13c5d5b213619d16add70ffc8942e00fa2c52f8
Kweku <nyarkokweku353@gmail.com>
2026-06-06 02:34:49 +0000

    Update project-2.py

--- project-2.py
+++ project-2.py
@@ -1,3 +1,9 @@
 first_name = input("What is your first name? ")
 last_name = input("What is your last name? ")
 print(f"Your name is {first_name} {last_name}.") #assignment 1a
+
+#greeting message
+print("Good day, welcome to the Goat Assignment!")
+pet = input("What is your pet's name? ")
+city = input("What city do you live in? ")
+print(f"Your new twitter handle and bio @cyber{pet} from {city}.") #assignment 2a
