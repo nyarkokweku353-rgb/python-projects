e36dae5db189139b178d0c51f80ec8b7402c8159
Kweku <nyarkokweku353@gmail.com>
2026-06-06 02:18:39 +0000

    Add IP input and print functionality

--- assignment.py
+++ assignment.py
@@ -4,3 +4,6 @@ print("Hello world\nHello world\nHello world") #assignment2
 
 name = "Kwkeu"
 print(f"{name} " * 10) #assignment3
+
+IP = input("Whats your IP ? ")
+print(f"Your IP is: {IP}") #assignment4
