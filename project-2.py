79dee704a1d7d831a8e7cf9d9884c4e84ad38900
Kweku <nyarkokweku353@gmail.com>
2026-06-07 10:04:24 +0000

    Update project-2.py

--- project-2.py
+++ project-2.py
@@ -1,3 +1,4 @@
+#print f and l name
 first_name = input("What is your first name? ")
 last_name = input("What is your last name? ")
 print(f"Your name is {first_name} {last_name}.") #assignment 1a
@@ -7,3 +8,35 @@ print("Good day, welcome to the Goat Assignment!")
 pet = input("What is your pet's name? ")
 city = input("What city do you live in? ")
 print(f"Your new twitter handle and bio @cyber{pet} from {city}.") #assignment 2a
+
+#scores 101
+print("Good day, welcome to the Goat Assignment3!")
+score = int(input("Score should be between 0 and 188: "))
+if score < 0 or score > 188:
+  print("Invalid score. Please enter a score between 0 and 188.")
+else:
+  if score >= 97:
+    print("Grade A")
+  elif score >= 77:
+    print("Grade B")
+  elif score >= 67:
+    print("Grade C")
+  else:
+    print("Grade D")
+
+#scores 102
+print("Good day, welcome to the Goat Assignment3!")
+score = int(input("Score should be between 0 and 188: "))
+if score < 0 or score > 188:
+  print("Invalid score. Please enter a score between 0 and 188.")
+else:
+  match score:
+    case s if s >= 97:
+        print("Grade A")
+    case s if s >= 77:
+      print("Grade B")
+    case s if s >= 67:
+      print("Grade C")
+    case _ :
+      print("Grade D")#assignment3
+
